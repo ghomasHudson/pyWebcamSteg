2838fedcf8e587b22e022b97b879b978d1c717fd
Thomas Hudson <g.t.hudson@durham.ac.uk>
2021-05-11 21:10:27 +0100

    Fix style

--- client.py
+++ client.py
@@ -13,40 +13,38 @@ import requests
 '''decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
 plain_text = decryption_suite.decrypt(cipher_text)'''
 
-
 proxyPort = 4125
 messageSize = 128
 messageQueue = []
 
 
 class proxyHandler(BaseHTTPRequestHandler):
+    # Handler for the GET requests
+    def do_GET(self):
+        # Load headers into message Queue
+        # 'Poke' server's buttons to alert it that steg data avalible at this IP
 
-	#Handler for the GET requests
-	def do_GET(self):
-		#Load headers into message Queue
-		#'Poke' server's buttons to alert it that steg data avalible at this IP
 
+        # TODO: remove
+        r = requests.get(self.path,stream=True,headers=self.headers)
+        for chunk in r.iter_content(100):
+            self.wfile.write(chunk)
 
-		#TODO: remove
-		r = requests.get(self.path,stream=True,headers=self.headers)
-		for chunk in r.iter_content(100):
-			self.wfile.write(chunk)
-
-		print("|",self.headers,"|")
-		# breakpoint()
-		messageQueue = [str(self.headers)[i:i+messageSize] for i in range(0, len(str(self.headers)), )]
-		print(messageQueue)
-		return
+        print("|",self.headers,"|")
+        # breakpoint()
+        messageQueue = [str(self.headers)[i:i+messageSize] for i in range(0, len(str(self.headers)), )]
+        print(messageQueue)
+        return
 
 try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', proxyPort), proxyHandler)
-	print(('Started webcam proxy on port' , proxyPort))
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', proxyPort), proxyHandler)
+    print(('Started webcam proxy on port' , proxyPort))
 
-	#Wait forever for incoming http requests
-	server.serve_forever()
+    # Wait forever for incoming http requests
+    server.serve_forever()
 
 except KeyboardInterrupt:
-	print('^C received, shutting down the web server')
-	server.socket.close()
+    print('^C received, shutting down the web server')
+    server.socket.close()
